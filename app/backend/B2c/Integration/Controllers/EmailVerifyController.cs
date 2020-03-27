84f8cd2c72879792855b7254df692d26eda9d55b
Kevin Hillinger <kevin.hillinger@microsoft.com>
2020-03-27 07:53:56 -0400

    email verify

---
+++ app/backend/B2c/Integration/Controllers/EmailVerifyController.cs
@@ -0,0 +1,47 @@
+using backend.B2c.Integration.Models;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleWebApp.B2c.Integration.Models;
+
+namespace SampleWebApp.B2c.Integration.Controllers
+{
+    [Authorize(AuthenticationSchemes = CertificateAuthenticationDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("b2c/[controller]")]
+    public class EmailVerifyController : ControllerBase
+    {
+        private readonly ILogger<UserProfileController> _logger;
+
+        public EmailVerifyController(ILogger<UserProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return BadRequest("Email address is required");
+            }
+
+            if (!DoesEmailExist(email))
+            {
+                return NotFound();
+            }
+
+            return Ok(new EmailVerifyResponse { Exists = true, Count = 1 });
+        }
+
+        private bool DoesEmailExist(string email)
+        {
+            return true;
+        }
+    }
+}
---
+++ app/backend/B2c/Integration/Models/EmailVerifyResponse.cs
@@ -0,0 +1,8 @@
+namespace backend.B2c.Integration.Models
+{
+    public class EmailVerifyResponse
+    {
+        public bool Exists { get; set; }
+        public int Count { get; set; }
+    }
+}
