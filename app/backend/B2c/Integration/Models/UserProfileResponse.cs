6d13765ab5943f7163b5fc084587ecc8a2467605
Kevin Hillinger <kevin.hillinger@microsoft.com>
2020-03-27 12:56:03 -0400

    removing roles controller and updating user profile response to include roles

--- app/backend/B2c/Integration/Models/UserProfileResponse.cs
+++ app/backend/B2c/Integration/Models/UserProfileResponse.cs
@@ -1,16 +1,14 @@
+using System.Collections.Generic;
+
 namespace SampleWebApp.B2c.Integration.Models
 {
     public class UserProfileResponse
     {
-        public string version { get; set; }
-        public int status { get; set; }
-        public string userMessage { get; set; }
-
         // Optional claims
-        public string email { get; set; }
-        public string emailHash { get; set; }
-        public string displayName { get; set; }
-        public string givenName { get; set; }
-        public string surName { get; set; }
+        public string Email { get; set; }        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<string> Roles {get;set;}
     }
 }
