5dc6c49003cb1e50451af89bbabf577c2ecd2a79
Kevin Hillinger <kevin.hillinger@microsoft.com>
2020-04-02 15:39:42 -0400

    script update

--- scripts/b2c-custom-ui-setup.sh
+++ scripts/b2c-custom-ui-setup.sh
@@ -10,7 +10,7 @@ az group create -n $resource_group -l $location  --output none
 
 echo "Creating storage account."
 
-az storage account create -n $storage_account_name 
+az storage account create -n $storage_account_name \
     -g $resource_group \
     -l $location \
     --sku Standard_LRS \
@@ -23,6 +23,15 @@ az storage container create \
     --name $storage_container \
     --public-access blob \
     --output none
+
+echo "Updating to allow for CORS."
+az storage cors add \
+    --methods GET OPTIONS \
+    --origins 'https://idhack007.b2clogin.com'
+    --services blob \
+    --account-key $(az storage account keys list --account-name $storage_account_name --resource-group $resource_group --output tsv --query '[?keyName == `key1`].value') \
+    --account-name $storage_account_name \
+    --allowed-headers * 
 
 echo "Deploying custom ui."
 az storage blob upload-batch \
